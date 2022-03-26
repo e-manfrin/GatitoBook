0ff6e7201915d24fc2fb5b08a7bbc760334945ad
Emily keyt Manfrin <rm87198@fiap.com.br>
2022-03-26 16:37:01 -0300

    Projeto final cadatro com login Angular

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,30 @@
+import { AutentificacaoService } from './../../autentificacao/autentificacao.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+    usuario = '';
+    senha = '';
+
+  constructor(private authService: AutentificacaoService, private router: Router) { }
+
+  ngOnInit(): void {}
+
+  login(){
+    this.authService.autenticar(this.usuario,this.senha).subscribe(
+      () => {
+      this.router.navigate(['animais']);
+    },
+    (error) => {
+          alert('Usuário ou senha inválido');
+          console.log(error);
+    }
+    );
+  }
+}
---
+++ src/app/home/novo-usuario/minusculo.validator.ts
@@ -0,0 +1,12 @@
+//Apens permita caracter minusculo
+
+import { AbstractControl } from "@angular/forms";
+
+export function minusculoValidator(control: AbstractControl){
+  const valor = control.value as string;
+  if (valor !== valor.toLowerCase()) {
+    return { minusculo: true};
+  } else {
+    return null;
+  }
+}
